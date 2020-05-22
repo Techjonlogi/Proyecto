7e95989a67cb7fef121af6062c3bf89910332f75
Alfredo Delgado Santiago <dltunasd@gmail.com>
2020-05-22 17:13:28 -0500

    Nuevas vistas agregadas

---
+++ Vistas/Administrador/MainAdministrador.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sistema_de_Prácticas_Profesionales.Vistas.Administrador
+{
+    /// <summary>
+    /// Lógica de interacción para MainAdministrador.xaml
+    /// </summary>
+    public partial class MainAdministrador : Window
+    {
+        public MainAdministrador()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonAdministrarCoordinadores_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
