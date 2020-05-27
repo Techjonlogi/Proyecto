3d10270d2bed02cb40d885d9b437f2c46c463ff4
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2020-05-26 22:41:16 -0500

    acomodando mas vistas

--- Vistas/RegistrarProyecto.xaml.cs
+++ Vistas/RegistrarProyecto.xaml.cs
@@ -15,6 +15,8 @@ using Sistema_de_Prácticas_Profesionales.Controller;
 using Sistema_de_Prácticas_Profesionales.Pojos.OrganizacionVinculada;
 using Sistema_de_Prácticas_Profesionales.Pojos.Coordinador;
 
+using Controller;
+
 namespace Sistema_de_Prácticas_Profesionales.Vistas
 {
     /// <summary>
@@ -26,10 +28,26 @@ namespace Sistema_de_Prácticas_Profesionales.Vistas
         {
             InitializeComponent();
             LlenarOrganizaciones();
-            LlenarCoordinadores():
+            LlenarCoordinadores();
 
 
         }
+
+        private enum ChecResults
+        {
+            Passed, Failed
+        }
+
+        public enum OperationResult
+        {
+            Success,
+            NullOrganization,
+            InvalidOrganization,
+            UnknowFail,
+            SQLFail,
+            ExistingRecord
+        }
+
 
         public void LlenarOrganizaciones()
         {
@@ -62,6 +80,95 @@ namespace Sistema_de_Prácticas_Profesionales.Vistas
         }
 
 
+        private ChecResults CheckEmptyFields()
+        {
+            ChecResults check = ChecResults.Failed;
+            if (textBoxClaveProyecto.Text == String.Empty || textBoxNombreProyecto.Text == String.Empty || comboOrganizaciones.Text == String.Empty || comboCordinador.Text == String.Empty || textBoxDuracion.Text == String.Empty || textBoxObjetivoGeneral.Text == String.Empty || textBoxDescripcion.Text == String.Empty || textBoxObjetivoMediato.Text == String.Empty || textBoxActividad.Text==String.Empty || textBoxMetodologia.Text==String.Empty || textBoxresponsabilidades.Text == String.Empty || textBoxCargoEncargado.Text== String.Empty || textBoxEmailEncargado.Text == String.Empty || textBoxNombreyapellidosResponsable.Text == String.Empty || textBoxRecursos.Text == String.Empty )
+            {
+                check = ChecResults.Failed;
+            }
+            else
+            {
+                check = ChecResults.Passed;
+            }
+            return check;
+        }
 
+
+
+
+
+
+
+
+
+
+
+
+        private ChecResults CheckFields()
+        {
+            ChecResults check = ChecResults.Failed;
+            Logica.CheckFields validarCampos = new Logica.CheckFields();
+            if (CheckEmptyFields() == ChecResults.Failed)
+            {
+                MessageBox.Show("Existen campos sin llenar");
+                check = ChecResults.Failed;
+            }
+            else if (validarCampos.ValidarMatricula(textBoxEmailEncargado.Text) == Logica.CheckFields.ResultadosValidación.Correoinválido)
+            {
+                MessageBox.Show("Formato de matricula incorrecto");
+            }
+            
+            else
+            {
+                check = ChecResults.Passed;
+            }
+            return check;
+        }
+
+        private void ComprobarResultado(OperationResult result)
+        {
+            if (result == OperationResult.Success)
+            {
+                MessageBox.Show("Añadido con exito");
+                this.Close();
+            }
+            else if (result == OperationResult.UnknowFail)
+            {
+                MessageBox.Show("Error desconocido");
+            }
+            else if (result == OperationResult.SQLFail)
+            {
+                MessageBox.Show("Error de la base de datos, intente mas tarde");
+            }
+            else if (result == OperationResult.ExistingRecord)
+            {
+                MessageBox.Show("Este Proyecto ya Existe");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void btnRegistrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckFields() == ChecResults.Passed)
+            {
+                ControladorProyecto proyectocontroller = new ControladorProyecto();
+                ComprobarResultado((OperationResult)proyectocontroller.AddProyecto(textBoxClaveProyecto.Text,textBoxresponsabilidades.Text,textBoxNombreProyecto.Text, textBoxDuracion.Text,textBoxDescripcion.Text,textBoxObjetivoGeneral.Text, textBoxObjetivoMediato.Text,textBoxCargoEncargado.Text,textBoxEmailEncargado.Text,textBoxNombreyapellidosResponsable.Text,textBoxMetodologia.Text,textBoxRecursos.Text,comboCordinador.Text, textBoxActividad.Text));
+            }
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
