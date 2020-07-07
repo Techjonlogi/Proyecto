079322567a72fb43791f8abf338c8ddde9010671
JONATHAN DE JESUS MORENO MARTINEZ <jhoni65475@gmail.com>
2020-07-06 22:28:34 -0500

    sincronizando el Explorador de archivos de windows para guardar mi pdf

--- Vistas/GenerarPdf.xaml.cs
+++ Vistas/GenerarPdf.xaml.cs
@@ -21,12 +21,14 @@ using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Kernel.Geom;
 using iText.Layout.Element;
+using Microsoft.Win32;
+using iText.Kernel.Pdf.Canvas.Parser.ClipperLib;
+
+
 
 namespace Sistema_de_Prácticas_Profesionales.Vistas
 {
-    /// <summary>
-    /// Lógica de interacción para GenerarPdf.xaml
-    /// </summary>
+   
     public partial class GenerarPdf : Window
     {
         public GenerarPdf()
@@ -58,16 +60,40 @@ namespace Sistema_de_Prácticas_Profesionales.Vistas
 
         private void GenerarArchivo(/*Proyecto proyecto,String nombreActividad, String actividad*/) 
         {
-            PdfWriter pw = new PdfWriter("C:\\demo.pdf");
-            PdfDocument pdfDocument = new PdfDocument(pw);
-            Document doc = new Document(pdfDocument, PageSize.LETTER);
-            doc.SetMargins(75, 35, 70, 35);
-            doc.Add(new iText.Layout.Element.Paragraph("hola"));
-            
-            doc.Close();
 
-          
-            
+            if (datagridProyecto.SelectedValue.ToString() != String.Empty)
+            {
+                String id;
+                ProfesorController controller = new ProfesorController();
+                id=((Proyecto)datagridProyecto.SelectedValue).IdProyecto;
+                UpdateGrid();
+            }
+            else { MessageBox.Show("Debe seleccionar un celda valida para eliminar"); }
+
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.FileName = "Document";
+            dialog.DefaultExt = ".Pdf";
+            dialog.Filter = "PDF Documents (.pdf)|*.pdf";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                String filename = dialog.FileName;
+
+
+
+                PdfWriter pw = new PdfWriter(filename);
+                PdfDocument pdfDocument = new PdfDocument(pw);
+                Document doc = new Document(pdfDocument, PageSize.A4);
+                doc.SetMargins(75, 35, 70, 35);
+                doc.Add(new iText.Layout.Element.Paragraph("hola"));
+
+                doc.Close();
+
+            }
+
+
 
         }
 
